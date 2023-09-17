14975b7718f836c1231fe7df5a0c5433d75fe5ac
Young Jin <likeRudin@gmail.com>
2023-09-17 15:09:10 +0900

    Logger setting
    NestFactory.create(AppModule) 로 nest app을 만듦.
    
    그 후 nest app에대한 실행  및 실행 로그 작성 책임이있는 클래스 Application을통해 app 서버를 실행.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+class Application {
+  private logger = new Logger(Application.name);
+  private PORT: string;
+
+  constructor(private app: INestApplication){
+    this.app = app;
+    this.PORT = process.env.PORT;
+  }
+
+  async bootstrap(){
+    await this.app.listen(this.PORT);
+  }
+
+  startLog(){
+    this.logger.log(
+      `Server is running at http://localhost:${this.PORT}`
+    );
+  }
+
 }
-bootstrap();
+
+const init = async () =>{
+  const server = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  const app = new Application(server);
+  await app.bootstrap();
+  app.startLog();
+}
+
+init();
