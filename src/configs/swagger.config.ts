6cb7c37b0fe59b8978a1fee0bf219979a4875e0c
Young Jin <likeRudin@gmail.com>
2023-09-17 16:58:17 +0900

    swagger api setting/ change nest to use express
    SwaggerAPI 문서를 생성하는 로직 추가

---
+++ src/configs/swagger.config.ts
@@ -0,0 +1,8 @@
+import { DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
+import { APP } from '@/constants';
+
+export const swaggerConfig = new DocumentBuilder()
+  .setTitle('ToDoIsMatter-backend API')
+  .setDescription('ToDoIsMatter Docs')
+  .setVersion(APP.VERSION)
+  .build();
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,45 @@
-import { INestApplication, Logger } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule } from '@nestjs/swagger';
+
+import expressBasicAuth from 'express-basic-auth';
+
+import { AppModule } from '@/app.module';
+import { API_URL } from '@/constants';
+import { swaggerConfig } from '@/configs/swagger.config';
 
 class Application {
   private logger = new Logger(Application.name);
   private PORT: string;
 
-  constructor(private app: INestApplication){
+  constructor(private app: NestExpressApplication){
     this.app = app;
     this.PORT = process.env.PORT;
   }
 
+  private async setUpOpenAPI() {
+    this.app.use([
+      API_URL.SWAGGER.DOCS,
+      API_URL.SWAGGER.DOCS_JSON
+      ],
+      expressBasicAuth({
+        challenge:true,
+        users: {
+          [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD,
+        },
+      }),
+    );
+    const document = SwaggerModule.createDocument(this.app, swaggerConfig);
+    SwaggerModule.setup(
+      API_URL.SWAGGER.DOCS,
+      this.app,
+      document,
+      );
+  }
+
   async bootstrap(){
+    await this.setUpOpenAPI();
     await this.app.listen(this.PORT);
   }
 
@@ -24,13 +52,13 @@ class Application {
 }
 
 const init = async () =>{
-  const server = await NestFactory.create(AppModule, {
-    bufferLogs: true,
-  });
-
+  const server = await NestFactory.create<NestExpressApplication>(
+    AppModule,{ bufferLogs: true, }
+    );
   const app = new Application(server);
   await app.bootstrap();
   app.startLog();
 }
 
 init();
+
