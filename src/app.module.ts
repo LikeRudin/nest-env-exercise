aac02da0acfa2117ecda183f63ec863d2d092200
Young Jin <likeRudin@gmail.com>
2023-09-17 17:16:41 +0900

    add typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { APP } from './constants';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP, CONFIG } from '@/constants';
+import { dbConfig } from '@/configs';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal:true,
       envFilePath: '.env.' + (process.env.NODE_ENV || APP.NODE_ENV.DEVELOPMENT),
+      load: [dbConfig],
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => config.get(CONFIG.DB),
+      inject: [ConfigService]
+    })
   ],
   controllers: [],
   providers: [],
