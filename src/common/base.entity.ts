c9cebb57d16261993ec703ebe9cf4f76b3e158a3
Young Jin <likeRudin@gmail.com>
2023-09-19 18:10:40 +0900

    add base entity

---
+++ src/common/base.entity.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+import { CreateDateColumn, DeleteDateColumn, UpdateDateColumn } from 'typeorm';
+
+export abstract class CommonEntity {
+  @ApiProperty({
+    example: '2023-09-19T00:00:00.000Z',
+    description: '생성일',
+    required: true,
+  })
+  @CreateDateColumn()
+  createdAt: Date;
+  @ApiProperty({
+    example: '2023-09-19T00:00:00.000Z',
+    description: '수정일',
+  })
+  @UpdateDateColumn()
+  updatedAt: Date; //👀
+
+  @ApiProperty({
+    example: '2023-09-19T00:00:00.000Z',
+    description: '삭제일',
+  })
+  @DeleteDateColumn()
+  deletedAt: Date;  //👀
+}
