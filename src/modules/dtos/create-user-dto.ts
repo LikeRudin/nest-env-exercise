a2c6da6d772a8e158d6fdb279e6d6b8bd79b1965
Young Jin <likeRudin@gmail.com>
2023-09-19 21:38:59 +0900

    add swagger doc to dtos

--- src/modules/dtos/create-user-dto.ts
+++ src/modules/dtos/create-user-dto.ts
@@ -1,5 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
+    @ApiProperty({
+        description: "이메일",
+        example: "todo@email.com"
+    })
     readonly email: string;
+    @ApiProperty({
+        description: "비밀번호",
+        example: "486"
+    })
     readonly password: string;
+    @ApiProperty({
+        description: "사용자이름",
+        example: "todoMania"
+    })
     readonly username: string;
 }
