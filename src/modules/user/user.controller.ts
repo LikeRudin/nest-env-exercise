5906fe11b42b788a5230f64f0de4afbb051baca8
Young Jin <likeRudin@gmail.com>
2023-09-21 20:23:30 +0900

    fix join logic : repository.save was needed

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,19 +1,27 @@
-import { Body, Controller, Get, Post, Req } from "@nestjs/common";
-import { CreateUserDto } from "../dtos/create-user-dto";
-import { UserService} from "./user.service";
-import { LoginUserDTO } from "../dtos/login-user-dto";
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { CreateUserDto } from '../dtos/create-user-dto';
+import { UserService } from './user.service';
+import { LoginUserDTO } from '../dtos/login-user-dto';
+import { Response } from 'express';
 @Controller('users')
 export class UserController {
-    
-    constructor(private readonly userService: UserService){}
-    
-    @Post('join')
-    async createUser(@Body() createUserData: CreateUserDto, @Req() req: Request): Promise<void>{
-        await this.userService.createEmailAccount(createUserData, req);
-    }
+  constructor(private readonly userService: UserService) {}
 
-    @Post('login')
-    async login(@Body() loginUserData: LoginUserDTO, @Req() req: Request): Promise<void>{
-        await this.userService.userLogin(loginUserData, req);
-    }
+  @Post('join')
+  async createUser(
+    @Body() createUserData: CreateUserDto,
+    @Req() req: Request,
+  ): Promise<void> {
+    await this.userService.createEmailAccount(createUserData);
+  }
+
+  @Post('login')
+  async login(
+    @Body() loginUserData: LoginUserDTO,
+    @Res() res: Response,
+  ): Promise<Record<string, any>> {
+    const token = await this.userService.userLogin(loginUserData);
+    res.cookie('token', token);
+    return res.status(200).send('logged in');
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,48 +1,56 @@
-import { HttpException, Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { CreateUserDto } from "../dtos/create-user-dto";
-import { Repository } from "typeorm";
-import { LoginUserDTO } from "../dtos/login-user-dto";
-import { UserEntity } from "./user.entity";
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from '../dtos/create-user-dto';
+import { Repository } from 'typeorm';
+import { LoginUserDTO } from '../dtos/login-user-dto';
+import { UserEntity } from './user.entity';
+import * as JWT from 'jsonwebtoken';
 
 @Injectable()
-export class UserService{
-    constructor(
-        @InjectRepository(UserEntity) 
-        private readonly userRepository: Repository<UserEntity>)
-        {}
-        
-    async createEmailAccount(userdata: CreateUserDto, req: Request){
-        const {username, email, password} = userdata;
-        const userAlreadyExist = await this.userRepository.find({where: [{email}]});
-        if (userAlreadyExist){
-            throw new HttpException(
-                "이미 가입한 이메일 입니다. ",
-                404
-                );
-        }
-        const user = await this.userRepository.create({
-            username,
-            email,
-            password
-        });
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
-        if (!user){
-            throw new HttpException(
-                "알 수 없는이유로 회원가입에 실패했습니다.",
-                502,
-            );
-        }
+  private createJWT(id: number) {
+    const token = JWT.sign({}, process.env.COOKIE_SECRET, {
+      algorithm: 'HS256',
+    });
+    return token;
+  }
+
+  async createEmailAccount(userdata: CreateUserDto) {
+    const { username, email, password } = userdata;
+    const userAlreadyExist = await this.userRepository.findOne({
+      where: { email },
+    });
+    if (userAlreadyExist) {
+      throw new HttpException('이미 가입한 이메일 입니다. ', 404);
     }
+    const user = this.userRepository.create({
+      username,
+      email,
+      password,
+    });
 
-    async userLogin(loginData: LoginUserDTO, req: Request){
-        const {email, password} = loginData;
-        const user = await this.userRepository.findOne({where:[{email}]});
-    
-    if(!user){
-        throw new HttpException("존재하지않는 사용자 이메일입니다", 402)
+    const userCreated = await this.userRepository.save(user);
+
+    if (!userCreated) {
+      throw new HttpException('알 수 없는이유로 회원가입에 실패했습니다.', 502);
+    }
+  }
+
+  async userLogin(loginData: LoginUserDTO) {
+    const { email, password } = loginData;
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new HttpException('존재하지않는 사용자 이메일입니다', 402);
     } else if (password !== user.password) {
-        throw new HttpException("잘못된 비밀번호 입니다.", 402)
+      throw new HttpException('잘못된 비밀번호 입니다.', 402);
     }
-    }
+
+    return this.createJWT(user.id);
+  }
 }
