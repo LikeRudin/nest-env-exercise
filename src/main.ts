868f0af96146b3496d950112e97ce8354a0786f2
Young Jin <likeRudin@gmail.com>
2023-09-21 20:24:01 +0900

    add dependencies: class-transformer, jsonwebtoken

--- src/main.ts
+++ src/main.ts
@@ -15,65 +15,51 @@ class Application {
   private logger = new Logger(Application.name);
   private PORT: string;
 
-  constructor(private app: NestExpressApplication){
+  constructor(private app: NestExpressApplication) {
     this.app = app;
     this.PORT = process.env.PORT;
   }
 
   private async setUpOpenAPI() {
-    this.app.use([
-      API_URL.SWAGGER.DOCS,
-      API_URL.SWAGGER.DOCS_JSON
-      ],
+    this.app.use(
+      [API_URL.SWAGGER.DOCS, API_URL.SWAGGER.DOCS_JSON],
       expressBasicAuth({
-        challenge:true,
+        challenge: true,
         users: {
           [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD,
         },
       }),
     );
     const document = SwaggerModule.createDocument(this.app, swaggerConfig);
-    SwaggerModule.setup(
-      API_URL.SWAGGER.DOCS,
-      this.app,
-      document,
-      );
+    SwaggerModule.setup(API_URL.SWAGGER.DOCS, this.app, document);
   }
 
-  private async setUpMiddlewares(){
-    this.app.use(cookieParser(process.env.COOKIE_SECRET,));
-    this.app.useGlobalPipes(new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }))
+  private async setUpMiddlewares() {
+    this.app.use(cookieParser(process.env.COOKIE_SECRET));
+    this.app.useGlobalPipes(new ValidationPipe());
   }
   private async setUpGlobalMiddleware() {
     this.setUpOpenAPI();
     this.setUpMiddlewares();
   }
 
-  async bootstrap(){
+  async bootstrap() {
     await this.setUpGlobalMiddleware();
     await this.app.listen(this.PORT);
   }
 
-  startLog(){
-    this.logger.log(
-      `Server is running at http://localhost:${this.PORT}`
-    );
+  startLog() {
+    this.logger.log(`Server is running at http://localhost:${this.PORT}`);
   }
-
 }
 
-const init = async () =>{
-  const server = await NestFactory.create<NestExpressApplication>(
-    AppModule,{ bufferLogs: true, }
-    );
+const init = async () => {
+  const server = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
   const app = new Application(server);
   await app.bootstrap();
   app.startLog();
-}
+};
 
 init();
-
