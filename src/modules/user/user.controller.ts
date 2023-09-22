95e8fbd9d892bcb0ab3519696fb9442a1ef458ce
Young Jin <likeRudin@gmail.com>
2023-09-22 19:58:56 +0900

    add swagger description on controller method

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,11 +4,13 @@ import { UserService } from './user.service';
 import { LoginUserDTO } from '../dtos/login-user-dto';
 import { Response } from 'express';
 import { ApiOperation, ApiParam } from '@nestjs/swagger';
+
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('join')
+  @ApiOperation({ summary: '회원가입' })
   async createUser(
     @Body() createUserData: CreateUserDto,
     @Res({ passthrough: true }) res: Response,
@@ -17,6 +19,7 @@ export class UserController {
   }
 
   @Post('login')
+  @ApiOperation({ summary: '로그인' })
   async login(
     @Body() loginUserData: LoginUserDTO,
     @Res({ passthrough: true }) res: Response,
@@ -27,9 +30,8 @@ export class UserController {
   }
 
   @Get(':id')
-  @ApiOperation({ summary: 'Get user info by ID' })
-  @ApiParam({ name: 'id', description: 'User ID' })
-  async getUserInfo(@Param(':id') userId) {
+  @ApiOperation({ summary: '유저 정보를 불러옵니다.' })
+  async getUserInfo(@Param(':id') userId: number) {
     const userData = await this.userService.getUserInfo(userId);
     return userData;
   }
