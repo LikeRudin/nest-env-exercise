13e0f3fe2538bbe326f05cfd50bf2d02ab6f721c
Young Jin <likeRudin@gmail.com>
2023-09-22 19:46:29 +0900

    change query method to findByOne : directly recieve where clause

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { CreateUserDto } from '../dtos/create-user-dto';
 import { UserService } from './user.service';
 import { LoginUserDTO } from '../dtos/login-user-dto';
 import { Response } from 'express';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -23,5 +24,13 @@ export class UserController {
     const token = await this.userService.userLogin(loginUserData);
     res.cookie('token', token);
     return { message: '로그인 성공' };
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get user info by ID' })
+  @ApiParam({ name: 'id', description: 'User ID' })
+  async getUserInfo(@Param(':id') userId) {
+    const userData = await this.userService.getUserInfo(userId);
+    return userData;
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,10 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from '../dtos/create-user-dto';
 import { Repository } from 'typeorm';
@@ -23,11 +29,11 @@ export class UserService {
 
   async createEmailAccount(userdata: CreateUserDto) {
     const { username, email, password } = userdata;
-    const userAlreadyExist = await this.userRepository.findOne({
-      where: [{ email }],
-    });
+    const userAlreadyExist = await this.userRepository.findOneBy({ email });
     if (userAlreadyExist) {
-      throw new HttpException('이미 가입한 이메일 입니다. ', 404);
+      throw new UnprocessableEntityException(
+        '이미 가입되어있는 이메일 입니다.',
+      );
     }
     const user = this.userRepository.create({
       username,
@@ -38,20 +44,35 @@ export class UserService {
     const userCreated = await this.userRepository.save(user);
 
     if (!userCreated) {
-      throw new HttpException('알 수 없는이유로 회원가입에 실패했습니다.', 502);
+      throw new HttpException(
+        '알 수 없는이유로 회원가입에 실패했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
   async userLogin(loginData: LoginUserDTO) {
     const { email, password } = loginData;
-    const user = await this.userRepository.findOne({ where: [{ email }] });
+    const user = await this.userRepository.findOneBy({ email });
 
     if (!user) {
-      throw new HttpException('존재하지않는 사용자 이메일입니다', 402);
+      throw new NotFoundException('존재하지 않는 사용자 이메일 입니다.');
     } else if (password !== user.password) {
-      throw new HttpException('잘못된 비밀번호 입니다.', 402);
+      throw new HttpException(
+        '잘못된 비밀번호 입니다.',
+        HttpStatus.NOT_ACCEPTABLE,
+      );
     }
 
     return this.createJWT(user.id);
+  }
+
+  async getUserInfo(id: number) {
+    const user = await this.userRepository.findOneBy({ id });
+
+    if (!user) {
+      throw new NotFoundException('유저가 존재하지 않습니다.');
+    }
+    return user;
   }
 }
